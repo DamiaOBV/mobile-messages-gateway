8d06b4818f65d4a4191ad6cad48a1856d650610b
damia.obrador <damia.obrador@e.andorratelecom.ad>
2022-12-06 14:04:50 +0100

    Initial commit

---
+++ src/main/java/com/example/mobilemessagesgateway/MobileMessagesGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.mobilemessagesgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileMessagesGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileMessagesGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mobilemessagesgateway/MobileMessagesGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mobilemessagesgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileMessagesGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
